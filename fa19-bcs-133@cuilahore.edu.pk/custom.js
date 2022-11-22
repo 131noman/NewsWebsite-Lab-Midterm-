79040ab6289de29ae5225a25ee5fa59f6b3d1ed0
Muhammad Noman <63111035+131noman@users.noreply.github.com>
2022-11-22 16:41:16 +0500

    commit

---
+++ fa19-bcs-133@cuilahore.edu.pk/custom.js
@@ -0,0 +1,10 @@
+
+
+
+/* I tried to write the JS code here but that was not working so I added at the end of html page
+* Please check in html page bottom.
+*
+* 1) OnClick event for "Sign In" link at top right corner. A modal will open after clicking on Sign in link/button.
+* 2) Added onmouseover and onmouseout events in "Breaking news" alert box (Changing box/div color)
+*
+* */
